a68b94c6eafbdf096ddb63c2d2531eaf8a25c62b
tunde <tabimbola1@studentmail.nait.ca>
2015-11-19 16:34:05 -0700

    added datemarker and time

--- eRestaurantDemo/eRestaurantWebsite/UXPages/FrontDesk.aspx.cs
+++ eRestaurantDemo/eRestaurantWebsite/UXPages/FrontDesk.aspx.cs
@@ -18,14 +18,7 @@ public partial class UXPages_FrontDesk : System.Web.UI.Page
     {
 
     }
-    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
-    {
-        AdminController sysmgr = new AdminController();
-        DateTime info = sysmgr.GetLastBillDateTime();
-        SearchDate.Text = info.ToString("yyyy-MM-dd");
-        SearchTime.Text = info.ToString("HH:mm");
-
-    }
+   
     protected void SeatingGridView_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
     {
         //extract tablenumber, waiterid, numberinparty from the gridview
@@ -48,7 +41,7 @@ public partial class UXPages_FrontDesk : System.Web.UI.Page
                 string tablenumber = (agvrow.FindControl("TableNumber") as Label).Text;
                 string numberinparty = (agvrow.FindControl("NumberInParty") as TextBox).Text;
                 string waiterid = (agvrow.FindControl("WaiterList") as DropDownList).SelectedValue;
-                DateTime when = DateTime.Parse(SearchDate.Text).Add(TimeSpan.Parse(SearchTime.Text));
+                DateTime when = Mocker.MockDate.Add(Mocker.MockTime);
 
                 //standard typical call to your controller in the BLL
                 AdminController sysmgr = new AdminController();
---
+++ eRestaurantDemo/eRestaurantWebsite/UserControls/DateTimeMarker.ascx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region additonal namespaces 
+using eRestaurantSystem.BLL;
+
+#endregion
+
+public partial class UserControls_DateTimeMarker : System.Web.UI.UserControl
+{
+    //create properties that will allow external users 
+    //of this control ro have access to the data 
+    //(date and time) on this control 
+
+    public DateTime MockDate
+    {
+        get
+        {
+      //set u a variable to hold the data 
+        // this variable will be innitalized to the defualt 
+            DateTime date = DateTime.MinValue;
+
+            //possible overribe the deufalut date wih the 
+            //contents of the web control SearchDate 
+
+            DateTime.TryParse(SearchDate.Text, out date);
+
+            //return date value.
+            return date;
+        }
+        set
+        {
+            SearchDate.Text = value.ToString("yyyy-MM-dd");
+
+        }
+    }
+
+
+    public TimeSpan MockTime
+    {
+        get
+        {
+            //set u a variable to hold the data 
+            // this variable will be innitalized to the defualt 
+            TimeSpan time = TimeSpan.MinValue;
+
+            //possible overribe the deufalut date wih the 
+            //contents of the web control SearchDate 
+
+            TimeSpan.TryParse(SearchTime.Text, out time);
+
+            //return date value.
+            return time;
+        }
+        set
+        {
+            SearchTime.Text = DateTime.Today.Add(value).ToString("HH:mm:ss");
+
+        }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
+    {
+        AdminController sysmgr = new AdminController();
+        DateTime info = sysmgr.GetLastBillDateTime();
+        SearchDate.Text = info.ToString("yyyy-MM-dd");
+        SearchTime.Text = info.ToString("HH:mm");
+
+    }
+}
